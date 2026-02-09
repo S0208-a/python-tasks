324f20015c9eb18b07f79834cef99d8666550525
S0208-a <alisamaira81@gmail.com>
2026-02-09 22:36:29 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,14 @@
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+#for addition
+add=num1+num2
+print("additiion is:",add)
+#for subtraction
+sub=num1-num2
+print("subtraction is:",sub)
+#for multiplication
+mul=num1*num2
+print("multiplication is:",mul)
+#for division
+div=num1/num2
+print("divsion is:",div)
---
+++ task2.py
@@ -0,0 +1,5 @@
+#Create a Personalized Greeting
+first_name=input("enter your first name:")
+last_name=input("enter your last name:")
+result=first_name+last_name
+print("hello",result,"welcome to python program")
